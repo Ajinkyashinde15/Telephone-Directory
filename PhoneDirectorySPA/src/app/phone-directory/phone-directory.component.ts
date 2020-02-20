54b3a10710ad6a9a9668d698c1f23027bf8dda6a
Ajinkya Shinde <ajinkyashinde15@gmail.com>
2020-02-20 23:07:59 +0000

    Adding form data to database done. Except notify user on GUI.

--- PhoneDirectorySPA/src/app/phone-directory/phone-directory.component.ts
+++ PhoneDirectorySPA/src/app/phone-directory/phone-directory.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormArray, Validators } from '@angular/forms';
+import { FormBuilder, FormArray, Validators, FormGroup } from '@angular/forms';
 import { PhoneService } from '../shared/phone.service';
+import { PhonebookService } from '../shared/phonebook.service';
 
 @Component({
   selector: 'app-phone-directory',
@@ -12,12 +13,19 @@ export class PhoneDirectoryComponent implements OnInit {
   phoneDirectoryForms : FormArray =this.fb.array([]);
   phoneBookCat = [];
 
-  constructor(private fb: FormBuilder, private phoneService : PhoneService) { }
+  constructor(private fb: FormBuilder, private phoneService : PhoneService, private phoneBookService : PhonebookService) { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
     this.phoneService.getPhoneBookCatList().subscribe( res=> this.phoneBookCat= res as []);
-
     this.addPhoneDirectoryForm();
+  }
+
+  recordSubmit(fg:FormGroup)
+  {
+    this.phoneBookService.postPhoneBookRecord(fg.value).subscribe(
+      (res : any) => {}
+    );
   }
 
   addPhoneDirectoryForm(){
--- PhoneDirectorySPA/src/app/shared/phonebook.service.ts
+++ PhoneDirectorySPA/src/app/shared/phonebook.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhonebookService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  postPhoneBookRecord(formData)
+  {
+    return this.http.post(environment.baseUrl+'phonebook',formData);
+  }
 }
