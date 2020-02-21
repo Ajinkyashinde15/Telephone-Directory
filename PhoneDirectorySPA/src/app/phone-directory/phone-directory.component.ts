bcede9eee242838bc7f4644710ae8aa15c90f960
Ajinkya Shinde <ajinkyashinde15@gmail.com>
2020-02-21 15:33:59 +0000

    Update and AddNew phone directory record work finished !!!

--- PhoneDirectorySPA/src/app/phone-directory/phone-directory.component.ts
+++ PhoneDirectorySPA/src/app/phone-directory/phone-directory.component.ts
@@ -18,16 +18,46 @@ export class PhoneDirectoryComponent implements OnInit {
   ngOnInit() 
   {
     this.phoneService.getPhoneBookCatList().subscribe( res=> this.phoneBookCat= res as []);
-    this.addPhoneDirectoryForm();
+    this.phoneBookService.getBankAccList().subscribe(
+      res =>{
+        if(res==[])
+        {
+          this.addPhoneDirectoryForm();
+        }else{
+          (res as []).forEach((phoneDirectory:any) => {
+
+            this.phoneDirectoryForms.push(this.fb.group({
+              phoneBookId : [phoneDirectory.phoneBookId],
+              firstName : [phoneDirectory.firstName,Validators.required ],
+              lastName : [phoneDirectory.lastName,Validators.required ],
+              address : [phoneDirectory.address,Validators.required ],
+              phoneNo : [phoneDirectory.phoneNo,Validators.required ],
+              email : [phoneDirectory.email,Validators.required ],
+              pbcId : [phoneDirectory.pbcId,Validators.min(1)]   
+            }));
+                   
+          });
+        }
+      }
+    );
   }
 
   recordSubmit(fg:FormGroup)
   {
-    this.phoneBookService.postPhoneBookRecord(fg.value).subscribe(
-      (res : any) => {
-        fg.patchValue({phoneBookId:res.phoneBookId});
-      }
-    );
+    if(fg.value.phoneBookId==0)
+    {      
+      this.phoneBookService.postPhoneBookRecord(fg.value).subscribe(
+        (res : any) => {
+          fg.patchValue({phoneBookId:res.phoneBookId});
+        }
+      );
+    }else
+    {
+      this.phoneBookService.putPhoneBookRecord(fg.value).subscribe(
+        (res : any) => {
+        }
+      );
+    }
   }
 
   addPhoneDirectoryForm(){
--- PhoneDirectorySPA/src/app/shared/phonebook.service.ts
+++ PhoneDirectorySPA/src/app/shared/phonebook.service.ts
@@ -10,8 +10,18 @@ export class PhonebookService {
 
   constructor(private http : HttpClient) { }
 
+  getBankAccList()
+  {
+    return this.http.get(environment.baseUrl + 'phonebook');
+  }
+
   postPhoneBookRecord(formData)
   {
     return this.http.post(environment.baseUrl+'phonebook',formData);
+  }
+
+  putPhoneBookRecord(formData)
+  {
+    return this.http.put(environment.baseUrl+'phonebook/'+formData.phoneBookId,formData);
   }
 }
